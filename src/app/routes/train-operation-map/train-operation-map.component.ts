9a4bc84b6b61422be2aa4621bec66bde5714631e
Wujae <wujae.sebastien@gmail.com>
2018-06-22 18:03:40 +0800

    代码提交

--- src/app/routes/train-operation-map/train-operation-map.component.ts
+++ src/app/routes/train-operation-map/train-operation-map.component.ts
@@ -1,7 +1,85 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {NgxEchartsService} from "ngx-echarts";
-import {zip} from "rxjs";
+import {zip, Observable, Subscription } from "rxjs";
+import {Observer} from "rxjs/internal/types";
+import {validateComponent} from "codelyzer/walkerFactory/walkerFn";
+
+const FETCH_CYCLE: number = 30 * 1000;
+const ANIME_CYCLE: number = 50; //以60HZ刷新率为基准
+const DEBOUNCE: number = 16;
+const ARRAY: Array<number> = [288, 13, 23, 322, 15, 23, 31, 124, 23, 100, 111, 56, 67, 99, 78, 90, 123, 23, 53, 51, 22, 1003, 23, 51, 41, 23];
+
+//用于迭代数据，更好的暂时效果
+class DataSource {
+  private lastFetchedNumber: number = 0;
+
+  private _idNumberRoll: any;
+  private _idInterval: any;
+
+  private count: number = 0;
+
+  ondata: Function;
+  oncomplete: Function;
+  onerror: Function;
+
+  constructor() {
+    this.fetchData();
+  }
+
+  private fetchData(){
+    //TODO 通过外部请求定时获取数据
+     this._idInterval = setInterval(()=> {
+
+      if(ARRAY[this.count]){
+        this.updateValue(ARRAY[this.count++]);
+      }else{
+        clearInterval(this._idInterval);
+      }
+    }, FETCH_CYCLE)
+
+
+  }
+
+  private updateValue(v: number){
+    let i = this.lastFetchedNumber, timeoutFlag = false;
+
+    //间隔太大，大于动画帧执行时间
+    if(Math.abs(v - i) > DEBOUNCE){
+      timeoutFlag = true;
+
+    }
+
+    if(i < v){
+      timeoutFlag ? i = v - DEBOUNCE : null;
+      this._idNumberRoll = setInterval(() => this.emit(i++), ANIME_CYCLE);
+    }else{
+      timeoutFlag ? i = v + DEBOUNCE : null;
+      this._idNumberRoll = setInterval(() => this.emit(i--), ANIME_CYCLE);
+    }
+
+    this.lastFetchedNumber = v;
+  }
+
+  //数据迭代
+  private emit(n) {
+    if (this.ondata) {
+      this.ondata(n);
+    }
+
+    if (n === this.lastFetchedNumber) {
+      if (this.oncomplete) {
+        this.oncomplete();
+      }
+      this.destroy();
+    }
+  }
+
+  destroy() {
+    clearInterval(this._idNumberRoll);
+
+  }
+}
 
 
 @Component({
@@ -11,18 +89,48 @@ import {zip} from "rxjs";
 })
 export class TrainOperationMapComponent implements OnInit {
 
-  warn: number;
+  warn: number = 0;
+  forecast: number = 0;
 
   // show loading spinner:
   mapLoaded = false;
   // empty option before geoJSON loaded:
   options = {};
 
+  warnDatasource: DataSource;
+
   constructor(private http: HttpClient, private es: NgxEchartsService) { }
+
+
+  private warnObservable(observer: Observer<number>) {
+
+      let warnDatasource = new DataSource();
+      warnDatasource.ondata = (e) => observer.next(e);
+      warnDatasource.onerror = (err) => observer.error(err);
+      warnDatasource.oncomplete = () => observer.complete();
+
+
+    return () => {
+      warnDatasource.destroy();
+    };
+  }
 
   ngOnInit() {
 
-    this.warn = 233;
+    let that = this;
+
+    let unsub = this.warnObservable({
+      next(x) {
+          that.warn = x;
+          that.forecast = x + Math.floor( Math.random() * 23)
+      },
+      error(err) {
+        console.error(err);
+        },
+      complete() {
+        console.log('done')
+      }
+    });
 
     zip(
       this.http.get(`assets/data/CHINA.json`),
@@ -32,42 +140,69 @@ export class TrainOperationMapComponent implements OnInit {
         // hide loading:
         this.mapLoaded = true;
 
-        console.log(this.es);
         // register map:
         this.es.registerMap('CHINA', geoJson);
 
         // update options:
         this.options = {
+          geo: {
+            map: 'CHINA',
+            label: {
+              emphasis: {
+                show: false
+              }
+            },
+            roam: true,
+            scaleLimit:{
+              min:1,
+              max:5
+            },
+            zoom:1.25,
+            itemStyle: {
+              normal: {
+                areaColor : "#329fec",
+                borderColor: '#eee'
+              },
+              emphasis: {
+                areaColor : "#20deec",
+              },
+            },
+            regions: provinceColor,
+          },
           series: [
             {
-              id:"CHINA_MAP",
-              type: 'map',
-              roam: true,
-              scaleLimit:{
-                min:1,
-                max:5
+              name: 'Top 5',
+              type: 'effectScatter',
+              mapType: 'CHINA', // map type should be registered
+              coordinateSystem: 'geo',
+              data: [
+                {name: '5501', value: [113,28.21], warn:12, forecast: 6},
+                {name: '5008', value: [118.88,28.97], warn:23, forecast: 13},
+                {name: '5654', value: [116.7,39.53], warn:1, forecast: 2},
+                {name: '5018', value: [115.480656,35.23375], warn:6, forecast: 2},
+                {name: '5372', value: [125.03,46.58], warn:7, forecast: 5},
+              ],
+              symbolSize: (val: any, dataRow) => (dataRow.data.warn + dataRow.data.forecast),
+              showEffectOn: 'render',
+              rippleEffect: {
+                brushType: 'stroke'
               },
-              zoom:1.25,
+              hoverAnimation: true,
               label: {
-                normal: {//通常情况下normal和emphasis是对应的,分别表示  “普通状态” 和 “选中状态” 下的样式.
-                  show: false//是否显示省会名字.
-                },
-                emphasis: {
+                normal: {
+                  formatter: (dataRow: any) => `${dataRow.data.name} ${dataRow.data.warn}/${dataRow.data.forecast}`,
+                  position: 'right',
                   show: true
                 }
               },
               itemStyle: {
                 normal: {
-                  areaColor : "#329fec",
-                  borderColor: '#eee'
-                },
-                emphasis: {
-                  areaColor : "#20deec",
-                },
+                  color: '#f4e925',
+                  shadowBlur: 10,
+                  shadowColor: '#333'
+                }
               },
-              mapType: 'CHINA', // map type should be registered
-              data: provinceColor
-
+              zlevel: 1
             }
           ]
         };
